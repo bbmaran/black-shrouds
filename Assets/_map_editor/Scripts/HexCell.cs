8cd9f3f23892c7b671d522fb6cfc07b8b6fb9bc4
bbmaran <35764002+bbmaran@users.noreply.github.com>
2018-01-24 16:05:45 +0300

    HexGrid pt1
    Added classes:
    HexCell - individual cell
    HexMetrics - cell inner/outer radius, corners and other math
    HexCoordinates - offset and box coordinates
    HexTileGrid - grid which creates cells
    HexCoordinatesDrawer - helper editor script for better visualisation of coordinates

---
+++ Assets/_map_editor/Scripts/HexCell.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour {
+
+	public PolygonCollider2D cellCollider;
+
+	public HexCoordinates coordinates;
+
+	void Awake() {
+		cellCollider = GetComponent<PolygonCollider2D>();
+
+		cellCollider.points = HexMetrics.corners;
+	}
+}
---
+++ Assets/_map_editor/Scripts/HexMetrics.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class HexMetrics {
+
+	public const float outerRadius = 4f;
+	public const float innerRadius = outerRadius * 0.866025404f;
+
+	public static Vector2[] corners = {
+		new Vector2(0f, outerRadius),
+		new Vector2(innerRadius, 0.5f * outerRadius),
+		new Vector2(innerRadius, -0.5f * outerRadius),
+		new Vector2(0f, -outerRadius),
+		new Vector2(-innerRadius, -0.5f * outerRadius),
+		new Vector2(-innerRadius, 0.5f * outerRadius)
+	};
+}
